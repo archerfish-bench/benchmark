9c98352112140ceb8cc05f9d760f27cf452aafe3
Rajesh Balamohan <rbalamohan@apache.org>
2023-11-09 09:42:42 +0530

    Add tokencounter and debug info in Lllama-index

--- src/benchmark/llama_index_benchmark.py
+++ src/benchmark/llama_index_benchmark.py
@@ -4,7 +4,9 @@ import os.path
 import time
 import traceback
 
+import tiktoken
 from llama_index import ServiceContext, SQLDatabase
+from llama_index.callbacks import TokenCountingHandler, CallbackManager, LlamaDebugHandler, CBEventType
 from llama_index.indices.struct_store import NLSQLTableQueryEngine
 from llama_index.llms import OpenAI
 from overrides import override
@@ -23,6 +25,8 @@ class LlamaIndexBenchmark(BenchmarkBase):
 
     def __init__(self, workload_data: dict, halt_on_error=False):
         super().__init__(workload_data, halt_on_error)
+        self.llama_debug = None
+        self.token_counter = None
         self.llama_index_query_engine = None
         self.table_names = None
 
@@ -39,8 +43,18 @@ class LlamaIndexBenchmark(BenchmarkBase):
             inspector = inspect(self.engine)
             self.table_names = inspector.get_table_names()
 
-            llm = OpenAI(temperature=0.0, model="gpt-4-0613")
-            service_context = ServiceContext.from_defaults(llm=llm)
+            model = "gpt-4-0613"
+            llm = OpenAI(temperature=0.0, model=model)
+
+            # setup callback handler for getting log info
+            self.token_counter = TokenCountingHandler(
+                tokenizer=tiktoken.encoding_for_model(model).encode
+            )
+            self.llama_debug = LlamaDebugHandler(print_trace_on_end=True)
+
+            callback_manager = CallbackManager([self.token_counter, self.llama_debug])
+
+            service_context = ServiceContext.from_defaults(llm=llm, callback_manager=callback_manager)
             sql_database = SQLDatabase(engine=self.engine)
 
             self.llama_index_query_engine = NLSQLTableQueryEngine(
@@ -51,6 +65,24 @@ class LlamaIndexBenchmark(BenchmarkBase):
         except Exception as ex:
             logging.error(f"Error while initializing llama-index: {ex}")
             raise ex
+
+    def __print_debug_info(self):
+        logging.info(f"Token counter: total tokens: {self.token_counter.total_embedding_token_count}")
+        print(
+            "Embedding Tokens: ",
+            self.token_counter.total_embedding_token_count,
+            "\n",
+            "LLM Prompt Tokens: ",
+            self.token_counter.prompt_llm_token_count,
+            "\n",
+            "LLM Completion Tokens: ",
+            self.token_counter.completion_llm_token_count,
+            "\n",
+            "Total LLM Token Count: ",
+            self.token_counter.total_llm_token_count,
+            "\n",
+        )
+        print(self.llama_debug.get_event_time_info(CBEventType.LLM))
 
     def generate_query(self, query_info: dict) -> TaskResult:
         """
@@ -75,6 +107,8 @@ class LlamaIndexBenchmark(BenchmarkBase):
             result.generated_query = response.metadata["sql_query"]
             result.is_query_generated = True
             result.generation_time = time.time() - stime
+            # Enable this on need basis
+            #self.__print_debug_info()
             return result
         except Exception as ex:
             # Handle other exceptions that may occur during query execution
